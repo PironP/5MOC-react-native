c0689cc5486f112d8055e125b7c14908c5025cee
Quentin Brunet <hello@quentinbrunet.com>
2020-04-01 01:20:14 +0200

    Rename the whole app

--- ios/CocktailAppTests/CocktailAppTests.m
+++ ios/CocktailAppTests/CocktailAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface projetReactNativeTests : XCTestCase
+@interface CocktailAppTests : XCTestCase
 
 @end
 
-@implementation projetReactNativeTests
+@implementation CocktailAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
